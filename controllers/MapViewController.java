457dfb304b4bc760da12e2fc39899e67f448d4d4
aivaraskriksciunas <aiviskri@gmail.com>
2022-05-20 11:45:42 +0200

    Map updates

--- controllers/MapViewController.java
+++ controllers/MapViewController.java
@@ -18,6 +18,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
@@ -26,9 +27,14 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import utils.NotifUtils;
 
 /**
@@ -41,8 +47,12 @@ public class MapViewController implements Initializable {
     enum MapAction {
         PLACING_POINT,
         PLACING_LINE,
+        PLACING_ARC,
+        PLACING_TEXT,
         EDITING_POINT,
         EDITING_LINE,
+        EDITING_ARC,
+        EDITING_TEXT,
         NONE,
     }
     
@@ -55,6 +65,10 @@ public class MapViewController implements Initializable {
     private ToggleButton pointUtilsBtn;
     @FXML
     private ToggleButton lineUtilsBtn;
+    @FXML
+    private ToggleButton arcUtilsBtn;
+    @FXML
+    private ToggleButton textUtilsBtn;
     @FXML
     private ToggleGroup utilSelection;
     
@@ -83,8 +97,6 @@ public class MapViewController implements Initializable {
     private ColorPicker editPointColorControl;
     @FXML
     private Slider editPointSizeControl;
-    @FXML
-    private Button removePoint;
     
     // Line editing controls
     @FXML
@@ -93,8 +105,45 @@ public class MapViewController implements Initializable {
     private ColorPicker editLineColorControl;
     @FXML
     private Slider editLineWidthControl;
+    
+    // Arc drawing controls
     @FXML
-    private Button removeLine;
+    private HBox arcOptions;
+    @FXML
+    private ColorPicker arcColorControl;
+    @FXML
+    private Slider arcWidthControl;
+    // Temporary arc
+    Arc drawingArc;
+    
+    // Text drawing controls
+    @FXML
+    private VBox textOptions;
+    @FXML
+    private ColorPicker textColorControl;
+    @FXML
+    private Slider textSizeControl;
+    @FXML
+    private TextField textValueControl;
+    private Text drawingText;
+    
+    // Arc edit controls
+    @FXML
+    private HBox editArcOptions;
+    @FXML
+    private ColorPicker editArcColorControl;
+    @FXML
+    private Slider editArcWidthControl;
+    
+    // Text edit controls
+    @FXML
+    private VBox editTextOptions;
+    @FXML
+    private ColorPicker editTextColorControl;
+    @FXML
+    private Slider editTextSizeControl;
+    @FXML
+    private TextField editTextValueControl;
     
     // Node that is currently selected for editing
     private Node selectedNode;
@@ -130,7 +179,6 @@ public class MapViewController implements Initializable {
         wrapGroup.getChildren().add( mapGroup );
         
         mapView.setContent( wrapGroup );
-        addCircle( 200, 200 );
         
         // Center map
         mapView.setHvalue( mapView.getHmax() / 2 );
@@ -155,12 +203,6 @@ public class MapViewController implements Initializable {
                 ((Circle)selectedNode).setRadius( nv.doubleValue() );
             }
         });
-        removePoint.setOnAction( a -> {
-            if ( selectedNode != null ) {
-                mapGroup.getChildren().remove( selectedNode );
-                currentAction.set( MapAction.NONE );
-            }
-        });
         
         // Bind listeners to line editing controls
         editLineColorControl.valueProperty().addListener( ( a, o, nv ) -> {
@@ -173,10 +215,50 @@ public class MapViewController implements Initializable {
                 ((Line)selectedNode).setStrokeWidth( nv.doubleValue() );
             }
         });
-        removeLine.setOnAction( a -> {
-            if ( selectedNode != null ) {
-                mapGroup.getChildren().remove( selectedNode );
-                currentAction.set( MapAction.NONE );
+        
+        // Bind listeners to arc editing controls
+        editArcColorControl.valueProperty().addListener( ( a, o, nv ) -> {
+            if ( selectedNode instanceof Arc ) {
+                ((Arc)selectedNode).setStroke( nv );
+            }
+        });
+        editArcWidthControl.valueProperty().addListener( ( a, o, nv ) -> {
+            if ( selectedNode instanceof Arc ) {
+                ((Arc)selectedNode).setStrokeWidth( nv.doubleValue() );
+            }
+        });
+        
+        // Bind listeners to text editing controls
+        editTextColorControl.valueProperty().addListener( ( a, o, nv ) -> {
+            if ( selectedNode instanceof Text ) {
+                ((Text)selectedNode).setFill( nv );
+            }
+        });
+        editTextSizeControl.valueProperty().addListener( ( a, o, nv ) -> {
+            if ( selectedNode instanceof Text ) {
+                ((Text)selectedNode).setFont( new Font( "System", nv.doubleValue() ) );
+            }
+        });
+        editTextValueControl.textProperty().addListener( ( a, o, nv ) -> {
+            if ( selectedNode instanceof Text ) {
+                ((Text)selectedNode).setText( nv );
+            }
+        });
+        
+        // Bind listeners to text drawing controls
+        textColorControl.valueProperty().addListener( ( a, o, nv ) -> {
+            if ( drawingText != null ) {
+                drawingText.setFill( nv );
+            }
+        });
+        textSizeControl.valueProperty().addListener( ( a, o, nv ) -> {
+            if ( drawingText != null ) {
+                drawingText.setFont( new Font( "System", nv.doubleValue() ) );
+            }
+        });
+        textValueControl.textProperty().addListener( ( a, o, nv ) -> {
+            if ( drawingText != null ) {
+                drawingText.setText( nv );
             }
         });
     }    
@@ -196,12 +278,26 @@ public class MapViewController implements Initializable {
         zoom( 1 );
     }
     
+    @FXML
+    private void onRemoveNode(ActionEvent event) {
+        if ( selectedNode != null ) {
+            mapGroup.getChildren().remove( selectedNode );
+            currentAction.set( MapAction.NONE );
+        }
+    }
+    
     private void onUtilChange( Toggle btn ) {
         if ( btn == pointUtilsBtn ) {
             currentAction.set( MapAction.PLACING_POINT );
         }
         else if ( btn == lineUtilsBtn ) {
             currentAction.set( MapAction.PLACING_LINE );
+        }
+        else if ( btn == arcUtilsBtn ) {
+            currentAction.set( MapAction.PLACING_ARC );
+        }
+        else if ( btn == textUtilsBtn ) {
+            currentAction.set( MapAction.PLACING_TEXT );
         }
         else {
             currentAction.set( MapAction.NONE );
@@ -266,37 +362,92 @@ public class MapViewController implements Initializable {
             mapGroup.setOnMouseMoved( null );
         }
     }
+    
+    private void addArc( double x, double y ) {
+        if ( drawingArc == null ) {
+            drawingArc = new Arc();
+            drawingArc.setCenterX( x );
+            drawingArc.setCenterY( y );
+            drawingArc.setRadiusX( 10 );
+            drawingArc.setRadiusY( 10 );
+            drawingArc.setType( ArcType.OPEN );
+            drawingArc.setStartAngle(0.0f); 
+            drawingArc.setLength(180.0f); 
+            drawingArc.setStroke( arcColorControl.getValue() );
+            drawingArc.setStrokeWidth( arcWidthControl.getValue() );
+            drawingArc.setFill( Color.TRANSPARENT );
+            
+            mapGroup.getChildren().add( drawingArc );
+            mapGroup.setOnMouseMoved( a -> updateArcEnd( a ) );
+        }
+        else {
+            drawingArc.setOnMouseClicked( ev -> onArcClicked( ev ) );
+            drawingArc = null;
+            mapGroup.setOnMouseMoved( null );
+        }
+    }
+    
+    private void startPlacingText() {
+        drawingText = new Text();
+        textValueControl.setText( "Text" );
+        drawingText.setX( 0 );
+        drawingText.setY( 0 );
+        drawingText.setFill( textColorControl.getValue() );
+        
+        drawingText.setFont( new Font( "System", textSizeControl.getValue() ) );
+        
+        mapGroup.getChildren().add( drawingText );
+        mapGroup.setOnMouseMoved( a -> updateText( a ) );
+    }
 
     private void onActionChange( MapAction action ) {
         if ( action != MapAction.EDITING_POINT && selectedNode != null ) {
             setSelectedNode( null );
         }
         
-        pointOptions.setVisible( false );
-        pointOptions.setManaged( false );
-        lineOptions.setVisible( false );
-        lineOptions.setManaged( false );
-        editPointOptions.setVisible( false );
-        editPointOptions.setManaged( false );
-        editLineOptions.setVisible( false );
-        editLineOptions.setManaged( false );
+        setNodeVisibility( pointOptions, false );
+        setNodeVisibility( lineOptions, false );
+        setNodeVisibility( editPointOptions, false );
+        setNodeVisibility( editLineOptions, false );
+        setNodeVisibility( arcOptions, false );
+        setNodeVisibility( editArcOptions, false );
+        setNodeVisibility( textOptions, false );
+        setNodeVisibility( editTextOptions, false );
         
-        if ( action == MapAction.PLACING_POINT ) {
-            pointOptions.setVisible( true );
-            pointOptions.setManaged( true );
+        if ( null != action ) switch (action) {
+            case PLACING_POINT:
+                setNodeVisibility( pointOptions, true );
+                break;
+            case PLACING_LINE:
+                setNodeVisibility( lineOptions, true );
+                break;
+            case EDITING_POINT:
+                setNodeVisibility( editPointOptions, true );
+                break;
+            case EDITING_LINE:
+                setNodeVisibility( editLineOptions, true );
+                break;
+            case PLACING_ARC:
+                setNodeVisibility( arcOptions, true );
+                break;
+            case EDITING_ARC:
+                setNodeVisibility( editArcOptions, true );
+                break;
+            case PLACING_TEXT:
+                setNodeVisibility( textOptions, true );
+                startPlacingText();
+                break;
+            case EDITING_TEXT:
+                setNodeVisibility( editTextOptions, true );
+                break;
+            default:
+                break;
         }
-        else if ( action == MapAction.PLACING_LINE ) {
-            lineOptions.setVisible( true );
-            lineOptions.setManaged( true );
-        }
-        else if ( action == MapAction.EDITING_POINT ) {
-            editPointOptions.setVisible( true );
-            editPointOptions.setManaged( true );
-        }
-        else if ( action == MapAction.EDITING_LINE ) {
-            editLineOptions.setVisible( true );
-            editLineOptions.setManaged( true );
-        }
+    }
+    
+    private void setNodeVisibility( Node node, boolean isVisible ) {
+        node.setVisible( isVisible );
+        node.setManaged( isVisible );
     }
     
     private void onMapMouseUp(MouseEvent event) {
@@ -312,11 +463,33 @@ public class MapViewController implements Initializable {
         else if ( currentAction.get() == MapAction.PLACING_LINE ) {
             addLine( event.getX(), event.getY() );
         }
+        else if ( currentAction.get() == MapAction.PLACING_ARC ) {
+            addArc( event.getX(), event.getY() );
+        }
+        else if ( currentAction.get() == MapAction.PLACING_TEXT ) {
+            if ( drawingText.getText().length() > 0 ) {
+                drawingText.setOnMouseClicked( e -> onTextClicked( e ) );
+                drawingText = null;
+                mapGroup.setOnMouseMoved( null );
+                currentAction.set( MapAction.NONE );
+                textUtilsBtn.setSelected( false );
+            }
+        }
     }
     
     private void updateLineEnd( MouseEvent event ) {
         drawingLine.setEndX( event.getX() );
         drawingLine.setEndY( event.getY() );
+    }
+    
+    private void updateArcEnd( MouseEvent event ) {
+        drawingArc.setRadiusX( Math.abs( drawingArc.getCenterX() - event.getX() ) );
+        drawingArc.setRadiusY( Math.abs( drawingArc.getCenterY() - event.getY() ) );
+    }
+    
+    private void updateText( MouseEvent event ) {
+        drawingText.setX( event.getX() );
+        drawingText.setY( event.getY() );
     }
     
     private void onPointClicked( MouseEvent event ) {
@@ -341,21 +514,53 @@ public class MapViewController implements Initializable {
         currentAction.set( MapAction.EDITING_LINE );
         
         setSelectedNode( (Node)event.getTarget() );
-        selectedNode.getStyleClass().add( "selected" );
+        selectedNode.getStyleClass().add( "selected-line" );
         
         editLineColorControl.setValue( (Color)c.getStroke());
         editLineWidthControl.setValue( c.getStrokeWidth() );
     }
     
+    private void onArcClicked( MouseEvent event ) {
+        if ( currentAction.get() != MapAction.NONE && !isEditing() ) return;
+        
+        Arc c = (Arc)event.getTarget();
+        
+        currentAction.set( MapAction.EDITING_ARC );
+        
+        setSelectedNode( (Node)event.getTarget() );
+        selectedNode.getStyleClass().add( "selected-line" );
+        
+        editArcColorControl.setValue( (Color)c.getStroke());
+        editArcWidthControl.setValue( c.getStrokeWidth() );
+    }
+    
+    private void onTextClicked( MouseEvent event ) {
+        if ( currentAction.get() != MapAction.NONE && !isEditing() ) return;
+        
+        Text t = (Text)event.getTarget();
+        
+        currentAction.set( MapAction.EDITING_TEXT );
+        setSelectedNode( t );
+        selectedNode.getStyleClass().add( "selected-line" );
+        
+        editTextColorControl.setValue( (Color)t.getFill() );
+        editTextSizeControl.setValue( t.getFont().getSize() );
+        editTextValueControl.setText( t.getText() );
+    }
+    
     private void setSelectedNode( Node nd ) {
         if ( selectedNode != null ) {
             selectedNode.getStyleClass().remove( "selected" );
+            selectedNode.getStyleClass().remove( "selected-line" );
         }
         selectedNode = nd;
     }
     
     private boolean isEditing() {
         return currentAction.get() == MapAction.EDITING_LINE || 
-                currentAction.get() == MapAction.EDITING_POINT;
+                currentAction.get() == MapAction.EDITING_POINT ||
+                currentAction.get() == MapAction.EDITING_ARC ||
+                currentAction.get() == MapAction.EDITING_TEXT;
     }
+    
 }
